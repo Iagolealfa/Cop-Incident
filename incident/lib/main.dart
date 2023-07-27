3e824a4abbdc503d63b7bd058d10995ba86d50c7
Iago Leal <70781193+Iagolealfa@users.noreply.github.com>
2023-07-27 10:45:59 -0300

    Criação do botão de novo incidente e tela de criação de novos incidentes

--- incident/lib/main.dart
+++ incident/lib/main.dart
@@ -22,22 +22,127 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Incident Watch'),
       ),
-      body: const Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const SizedBox(
+          SizedBox(
             width: double.infinity,
-            child: const Card(
-              color: Colors.blue,
-              child: Text('Gráfico de calor dos incidentes'),
-              elevation: 5,
-            ),
+            child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Gráfico de calor dos incidentes')),
           ),
-          const Card(
-            child: Text('Botão para criação de incidente'),
-          ),
+          Card(
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CreateIncidentScreen()),
+                    );
+                  },
+                  child: Text('Criar Incidente'))),
         ],
+      ),
+    );
+  }
+}
+
+class CreateIncidentScreen extends StatefulWidget {
+  @override
+  _CreateIncidentScreenState createState() => _CreateIncidentScreenState();
+}
+
+class _CreateIncidentScreenState extends State<CreateIncidentScreen> {
+  String nome = '';
+  int idade = 0;
+  String genero = '';
+  String raca = '';
+  String endereco = '';
+  String descricao = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criar Incidente'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Seu Nome'),
+              onChanged: (value) {
+                setState(() {
+                  nome = value;
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Sua Idade'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  idade = int.tryParse(value) ?? 0;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Sua Raça'),
+              onChanged: (value) {
+                setState(() {
+                  raca = value;
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Seu Gênero'),
+              onChanged: (value) {
+                setState(() {
+                  genero = value;
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Endereço do Incidente'),
+              onChanged: (value) {
+                setState(() {
+                  endereco = value;
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Descrição do Incidente'),
+              onChanged: (value) {
+                setState(() {
+                  descricao = value;
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Implemente aqui a lógica para submeter os dados do formulário.
+                // Por exemplo, enviar os dados para um servidor ou salvá-los localmente.
+                // Para esta demonstração, apenas exibiremos os dados no console.
+                print('Nome: $nome');
+                print('Idade: $idade');
+                print('Raça: $raca');
+                print('Gênero: $genero');
+                print('Endereço: $endereco');
+                print('Descrição: $descricao');
+              },
+              child: Text('Comitar Incidente'),
+            ),
+          ],
+        ),
       ),
     );
   }
