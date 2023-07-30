72d0b7bbc46cd481103d749450651b33867a474f
Iago Leal <70781193+Iagolealfa@users.noreply.github.com>
2023-07-30 14:55:34 -0300

    tirar o bunner debug

--- incident/lib/main.dart
+++ incident/lib/main.dart
@@ -24,6 +24,7 @@ class IncidentApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => MyHomePage(),
