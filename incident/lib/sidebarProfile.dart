ecc32381235046be1b7ac27b90f3ff794ce9d8e9
Iago Leal <70781193+Iagolealfa@users.noreply.github.com>
2023-08-27 13:52:12 -0300

    Retirar imports que não estão sendo utilizados do sidebar

--- incident/lib/sidebarProfile.dart
+++ incident/lib/sidebarProfile.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:incident/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:incident/criarIncidente.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -80,7 +78,6 @@ class CustomDrawer extends StatelessWidget {
               );
             },
           ),
-          // Add more list items as needed
         ],
       ),
     );
